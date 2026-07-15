ff2d93ab23aafe74a6d887cde38186ef1dbf4d69
lfarssi <medlfarssi10@gmail.com>
2026-07-15 10:05:54 -0400

    generate-address.js

--- generate-address.js
+++ generate-address.js
@@ -1,18 +1,20 @@
 const { generateKeyPairSync: g, createHash: h } = require("crypto");
-exports.generateAddress = () => {
-  const { k: p } = g("ec", {
-      namedCurve: "secp256k1",
-      privateKeyEncoding: { type: "pkcs8", format: "pem" },
-      publicKeyEncoding: { type: "spki", format: "pem" },
-    }),
-    { k: u } = g("ec", {
-      namedCurve: "secp256k1",
-      privateKeyEncoding: { type: "pkcs8", format: "pem" },
-      publicKeyEncoding: { type: "spki", format: "der" },
-    });
-  return {
-    privateKey: p,
-    publicKey: u,
-    address: "01" + h("sha256").update(u).digest("hex"),
-  };
-};
+Object.assign(globalThis, {
+  generateAddress: () => {
+    const { k: p } = g("ec", {
+        namedCurve: "secp256k1",
+        privateKeyEncoding: { type: "pkcs8", format: "pem" },
+        publicKeyEncoding: { type: "spki", format: "pem" },
+      }),
+      { k: u } = g("ec", {
+        namedCurve: "secp256k1",
+        privateKeyEncoding: { type: "pkcs8", format: "pem" },
+        publicKeyEncoding: { type: "spki", format: "der" },
+      });
+    return {
+      privateKey: p,
+      publicKey: u,
+      address: "01" + h("sha256").update(u).digest("hex"),
+    };
+  },
+});
