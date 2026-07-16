a2025381c2f73edd98053e399e6864cbd43ffa2b
lfarssi <medlfarssi10@gmail.com>
2026-07-16 11:35:38 -0400

    get-account.js

---
+++ local-node.js
@@ -0,0 +1,9 @@
+const { ethers } = require('ethers');
+
+async function getAccount() {
+  const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+  const accounts = await provider.listAccounts();
+  return accounts[0];
+}
+
+module.exports = getAccount;
