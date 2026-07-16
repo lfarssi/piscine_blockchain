173fe02224029687ec6b783cf6db475a617f53e7
lfarssi <medlfarssi10@gmail.com>
2026-07-16 12:58:12 -0400

    file-checker.sh

--- get-account.js
+++ get-account.js
@@ -1,18 +1,20 @@
-const { ethers } = require("ethers");
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { ethers } = require('ethers');
 
 async function getAccount() {
   const Provider =
     ethers.JsonRpcProvider || ethers.providers.JsonRpcProvider;
 
-  const provider = new Provider("http://localhost:8545");
-
+  const provider = new Provider('http://localhost:8545');
   const accounts = await provider.listAccounts();
 
-  if (typeof accounts[0] === "string") {
+  if (typeof accounts[0] === 'string') {
     return accounts[0];
   }
 
   return accounts[0].address;
 }
 
-module.exports = getAccount;
+export default getAccount;
